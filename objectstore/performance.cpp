5e2e7bc22e5514a4abc29344024ad93820a962a5
xinzheyang <xinzhe.y@gmail.com>
2019-02-28 15:03:19 -0500

    performace file

--- objectstore/performance.cpp
+++ objectstore/performance.cpp
@@ -15,24 +15,28 @@ int main(int argc, char** argv) {
     // print some message
     std::cout << "Object store service started. Is replica:" << std::boolalpha << oss.isReplica()
               << std::noboolalpha << "." << std::endl;
+
     bool use_aio = true;
     if(strcmp("aio", argv[1]) != 0) {
         use_aio = false;
     }
+
     int runtime = 60 * 1000;  // approximate runtime
     int num_msg = 10000;      // num_msg sent for the trial run
     uint64_t max_msg_size = derecho::getConfUInt64(CONF_DERECHO_MAX_PAYLOAD_SIZE);
     int msg_size = max_msg_size - 128;
+
     char odata[msg_size];
     for(int i = 0; i < msg_size; i++) {
         odata[i] = 'A';
     }
-
+    
+    // create a pool of objects
     std::vector<objectstore::Object> objpool;
     for(int i = 0; i < num_msg; i++) {
         objpool.push_back(objectstore::Object(i, odata, msg_size + 1));
     }
-    
+
     // trial run to get an approximate number of objects to reach runtime
     clock_gettime(CLOCK_REALTIME, &t_start);
     if(use_aio) {
@@ -49,17 +53,18 @@ int main(int argc, char** argv) {
 
     long long int nsec = (t_end.tv_sec - t_start.tv_sec) * 1000000000 + (t_end.tv_nsec - t_start.tv_nsec);
     double msec = (double)nsec / 1000000;
-    std::cout << msec<< std::endl;
-    int multiplier = ceil(runtime/msec);
-	std::cout << multiplier << std::endl;
+
+    int multiplier = ceil(runtime / msec);
+    
+    // real benchmarking starts
     clock_gettime(CLOCK_REALTIME, &t_start);
     if(use_aio) {
         for(int i = 0; i < num_msg * multiplier; i++) {
-            oss.aio_put(objpool[i]);
+            oss.aio_put(objpool[i % num_msg]);
         }
     } else {
         for(int i = 0; i < num_msg * multiplier; i++) {
-            oss.bio_put(objpool[i]);
+            oss.bio_put(objpool[i % num_msg]);
         }
     }
     oss.bio_get(num_msg - 1);
@@ -67,8 +72,8 @@ int main(int argc, char** argv) {
 
     nsec = (t_end.tv_sec - t_start.tv_sec) * 1000000000 + (t_end.tv_nsec - t_start.tv_nsec);
     msec = (double)nsec / 1000000;
-    double thp_mBps = ((double)max_msg_size * num_msg * 1000) / nsec;
-    double thp_ops = ((double)num_msg * 1000000000) / nsec;
+    double thp_mBps = ((double)max_msg_size * num_msg * multiplier * 1000) / nsec;
+    double thp_ops = ((double)num_msg * multiplier * 1000000000) / nsec;
     std::cout << "timespan:" << msec << " millisecond." << std::endl;
     std::cout << "throughput:" << thp_mBps << "MB/s." << std::endl;
     std::cout << "throughput:" << thp_ops << "op/s." << std::endl;
