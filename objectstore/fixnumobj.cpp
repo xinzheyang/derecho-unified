40bff4b91366612f1f02b4681f12cb49e9f0b260
xinzheyang <xinzhe.y@gmail.com>
2019-04-07 22:36:28 -0400

    support nonsending node

--- objectstore/fixnumobj.cpp
+++ objectstore/fixnumobj.cpp
@@ -2,11 +2,11 @@
 #include "conf/conf.hpp"
 #include <iostream>
 #include <time.h>
-#define NUM_APP_ARGS (2)
+#define NUM_APP_ARGS (3)
 
 int main(int argc, char** argv) {
     if((argc < (NUM_APP_ARGS + 1)) || ((argc > (NUM_APP_ARGS + 1)) && strcmp("--", argv[argc - NUM_APP_ARGS - 1]))) {
-        std::cerr << "Usage: " << argv[0] << " [ derecho-config-list -- ] <aio|bio> num_objs" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " [ derecho-config-list -- ] <aio|bio> num_objs issender(0-not sending, 1-sending)" << std::endl;
         return -1;
     }
 
@@ -21,51 +21,63 @@ int main(int argc, char** argv) {
     derecho::Conf::initialize(argc, argv);
     std::cout << "Starting object store service..." << std::endl;
     // oss - objectstore service
+
+    bool issender = std::stoi(argv[argc - 1]);
+    int counter_nonsender = 0;
+
     auto& oss = objectstore::IObjectStoreService::getObjectStoreService(argc, argv,
                                                                         [&](const objectstore::OID& oid, const objectstore::Object& object) {
-                                                                            //std::cout << "watcher: " << oid << "->" << object << std::endl;
+                                                                            if(!issender) {
+                                                                                counter_nonsender++;
+                                                                            }
                                                                         });
     // print some message
     std::cout << "Object store service started. Is replica:" << std::boolalpha << oss.isReplica()
               << std::noboolalpha << "." << std::endl;
 
-    int num_msg = std::stoi(argv[argc - 1]);  // num_msg sent for the trial run
+    int num_msg = std::stoi(argv[argc - 2]);
     uint64_t max_msg_size = derecho::getConfUInt64(CONF_DERECHO_MAX_PAYLOAD_SIZE);
     int msg_size = max_msg_size - 128;
 
-    char odata[msg_size];
-    srand(time(0));
-    for(int i = 0; i < msg_size; i++) {
-        odata[i] = '1' + (rand() % 74);
-    }
-    // create a pool of objects
-    std::vector<objectstore::Object> objpool;
-    for(int i = 0; i < num_msg; i++) {
-        objpool.push_back(objectstore::Object(i, odata, msg_size + 1));
-    }
-
-    // trial run to get an approximate number of objects to reach runtime
-    clock_gettime(CLOCK_REALTIME, &t_start);
-    if(use_aio) {
-        for(int i = 0; i < num_msg; i++) {
-            oss.aio_put(objpool[i]);
+    if(issender) {
+        char odata[msg_size];
+        srand(time(0));
+        for(int i = 0; i < msg_size; i++) {
+            odata[i] = '1' + (rand() % 74);
         }
+        // create a pool of objects
+        std::vector<objectstore::Object> objpool;
+        for(int i = 0; i < num_msg; i++) {
+            objpool.push_back(objectstore::Object(i, odata, msg_size + 1));
+        }
+
+        // trial run to get an approximate number of objects to reach runtime
+        clock_gettime(CLOCK_REALTIME, &t_start);
+        if(use_aio) {
+            for(int i = 0; i < num_msg; i++) {
+                oss.aio_put(objpool[i]);
+            }
+        } else {
+            for(int i = 0; i < num_msg; i++) {
+                oss.bio_put(objpool[i]);
+            }
+        }
+        oss.bio_get(num_msg - 1);
+        clock_gettime(CLOCK_REALTIME, &t_end);
+
+        long long int nsec = (t_end.tv_sec - t_start.tv_sec) * 1000000000 + (t_end.tv_nsec - t_start.tv_nsec);
+        double msec = (double)nsec / 1000000;
+
+        double thp_mBps = ((double)max_msg_size * num_msg * 1000) / nsec;
+        double thp_ops = ((double)num_msg * 1000000000) / nsec;
+        std::cout << "timespan:" << msec << " millisecond." << std::endl;
+        std::cout << "throughput:" << thp_mBps << "MB/s." << std::endl;
+        std::cout << "throughput:" << thp_ops << "op/s." << std::endl;
+        std::cout << std::flush;
+        oss.leave();
     } else {
-        for(int i = 0; i < num_msg; i++) {
-            oss.bio_put(objpool[i]);
+        while(num_msg != counter_nonsender) {
         }
+        oss.leave();
     }
-    oss.bio_get(num_msg - 1);
-    clock_gettime(CLOCK_REALTIME, &t_end);
-
-    long long int nsec = (t_end.tv_sec - t_start.tv_sec) * 1000000000 + (t_end.tv_nsec - t_start.tv_nsec);
-    double msec = (double)nsec / 1000000;
-
-    double thp_mBps = ((double)max_msg_size * num_msg * 1000) / nsec;
-    double thp_ops = ((double)num_msg * 1000000000) / nsec;
-    std::cout << "timespan:" << msec << " millisecond." << std::endl;
-    std::cout << "throughput:" << thp_mBps << "MB/s." << std::endl;
-    std::cout << "throughput:" << thp_ops << "op/s." << std::endl;
-    std::cout << std::flush;
-    oss.leave();
 }
